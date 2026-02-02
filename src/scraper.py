445fba900b1051cfe63a01280d302fb03d9d177a
ihunna orji <ihunnaemmanuel@gmail.com>
2026-02-02 23:45:25 +0100

    updated scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -231,7 +231,7 @@ class Scraper:
                                     'division_id': division_id,
                                     'division': division_name,
                                     'season_id': season_id,
-                                    'season': f'({season_name})'
+                                    'season': season_name
                                 })
                     return team_list
                 except Exception as e:
